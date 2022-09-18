3cdf0d5db61af718162597d337d3a11d3c5b6c01
Fatma Şenay Çiftçi <f.senay.yilmaz@hotmail.com>
2022-09-18 15:25:44 +0300

    1809 Repository Giriş

---
+++ WeightGain.DAL/Repositories/BaseModelRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightGain.DAL.Repositories
+{
+    public class BaseModelRepository<T> where T:class
+    {
+        protected readonly WeightGainContext _dbContext;
+    }
+}
