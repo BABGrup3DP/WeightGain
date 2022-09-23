32efe99ed27378d59999d95c0c426e98596814af
Çağatay Korkut <korkutc4@gmail.com>
2022-09-23 12:07:49 +0300

    .

--- WeightGain.UI/UserForms/MainForm.Designer.cs
+++ WeightGain.UI/UserForms/MainForm.Designer.cs
@@ -28,37 +28,251 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblUser = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel4 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel5 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel6 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel7 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel8 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.txtAgeText = new System.Windows.Forms.TextBox();
+            this.txtWeightText = new System.Windows.Forms.TextBox();
+            this.txtHeightText = new System.Windows.Forms.TextBox();
+            this.txtBMH = new System.Windows.Forms.TextBox();
+            this.txtGetCalory = new System.Windows.Forms.TextBox();
+            this.lblDateToday = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel10 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.leftPanelMenuLogo = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2HtmlLabel11 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPanelMenuLogo)).BeginInit();
             this.SuspendLayout();
             // 
-            // guna2Button1
+            // pictureBox1
             // 
-            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.guna2Button1.ForeColor = System.Drawing.Color.White;
-            this.guna2Button1.Location = new System.Drawing.Point(346, 199);
-            this.guna2Button1.Name = "guna2Button1";
-            this.guna2Button1.Size = new System.Drawing.Size(180, 45);
-            this.guna2Button1.TabIndex = 0;
-            this.guna2Button1.Text = "guna2Button1";
+            this.pictureBox1.Image = global::WeightGain.UI.Properties.Resources.Female_User_Account_PNG_Photo;
+            this.pictureBox1.Location = new System.Drawing.Point(321, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 201);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblUser
+            // 
+            this.lblUser.BackColor = System.Drawing.Color.Transparent;
+            this.lblUser.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblUser.Location = new System.Drawing.Point(382, 219);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(71, 22);
+            this.lblUser.TabIndex = 1;
+            this.lblUser.Text = "Kullanıcı1";
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(321, 324);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(57, 19);
+            this.guna2HtmlLabel2.TabIndex = 2;
+            this.guna2HtmlLabel2.Text = "Kilonuz :";
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(321, 365);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(64, 19);
+            this.guna2HtmlLabel3.TabIndex = 2;
+            this.guna2HtmlLabel3.Text = "Boyunuz :";
+            // 
+            // guna2HtmlLabel4
+            // 
+            this.guna2HtmlLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel4.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.guna2HtmlLabel4.Location = new System.Drawing.Point(321, 280);
+            this.guna2HtmlLabel4.Name = "guna2HtmlLabel4";
+            this.guna2HtmlLabel4.Size = new System.Drawing.Size(54, 19);
+            this.guna2HtmlLabel4.TabIndex = 2;
+            this.guna2HtmlLabel4.Text = "Yaşınız :";
+            // 
+            // guna2HtmlLabel5
+            // 
+            this.guna2HtmlLabel5.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel5.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.guna2HtmlLabel5.Location = new System.Drawing.Point(194, 412);
+            this.guna2HtmlLabel5.Name = "guna2HtmlLabel5";
+            this.guna2HtmlLabel5.Size = new System.Drawing.Size(148, 19);
+            this.guna2HtmlLabel5.TabIndex = 3;
+            this.guna2HtmlLabel5.Text = "Vücut Kitle Endeksiniz :";
+            // 
+            // guna2HtmlLabel6
+            // 
+            this.guna2HtmlLabel6.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel6.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.guna2HtmlLabel6.Location = new System.Drawing.Point(180, 457);
+            this.guna2HtmlLabel6.Name = "guna2HtmlLabel6";
+            this.guna2HtmlLabel6.Size = new System.Drawing.Size(162, 19);
+            this.guna2HtmlLabel6.TabIndex = 4;
+            this.guna2HtmlLabel6.Text = "Günlük Kalori İhtiyacınız :";
+            // 
+            // guna2HtmlLabel7
+            // 
+            this.guna2HtmlLabel7.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel7.Location = new System.Drawing.Point(448, 461);
+            this.guna2HtmlLabel7.Name = "guna2HtmlLabel7";
+            this.guna2HtmlLabel7.Size = new System.Drawing.Size(29, 15);
+            this.guna2HtmlLabel7.TabIndex = 5;
+            this.guna2HtmlLabel7.Text = "Kalori";
+            // 
+            // guna2HtmlLabel8
+            // 
+            this.guna2HtmlLabel8.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel8.Location = new System.Drawing.Point(448, 417);
+            this.guna2HtmlLabel8.Name = "guna2HtmlLabel8";
+            this.guna2HtmlLabel8.Size = new System.Drawing.Size(27, 15);
+            this.guna2HtmlLabel8.TabIndex = 5;
+            this.guna2HtmlLabel8.Text = "BMH";
+            // 
+            // txtAgeText
+            // 
+            this.txtAgeText.Location = new System.Drawing.Point(391, 275);
+            this.txtAgeText.Name = "txtAgeText";
+            this.txtAgeText.ReadOnly = true;
+            this.txtAgeText.Size = new System.Drawing.Size(94, 20);
+            this.txtAgeText.TabIndex = 6;
+            // 
+            // txtWeightText
+            // 
+            this.txtWeightText.Location = new System.Drawing.Point(391, 323);
+            this.txtWeightText.Name = "txtWeightText";
+            this.txtWeightText.ReadOnly = true;
+            this.txtWeightText.Size = new System.Drawing.Size(94, 20);
+            this.txtWeightText.TabIndex = 6;
+            // 
+            // txtHeightText
+            // 
+            this.txtHeightText.Location = new System.Drawing.Point(391, 364);
+            this.txtHeightText.Name = "txtHeightText";
+            this.txtHeightText.ReadOnly = true;
+            this.txtHeightText.Size = new System.Drawing.Size(94, 20);
+            this.txtHeightText.TabIndex = 6;
+            // 
+            // txtBMH
+            // 
+            this.txtBMH.Location = new System.Drawing.Point(348, 412);
+            this.txtBMH.Name = "txtBMH";
+            this.txtBMH.ReadOnly = true;
+            this.txtBMH.Size = new System.Drawing.Size(94, 20);
+            this.txtBMH.TabIndex = 6;
+            // 
+            // txtGetCalory
+            // 
+            this.txtGetCalory.Location = new System.Drawing.Point(348, 457);
+            this.txtGetCalory.Name = "txtGetCalory";
+            this.txtGetCalory.ReadOnly = true;
+            this.txtGetCalory.Size = new System.Drawing.Size(94, 20);
+            this.txtGetCalory.TabIndex = 6;
+            // 
+            // lblDateToday
+            // 
+            this.lblDateToday.BackColor = System.Drawing.Color.Transparent;
+            this.lblDateToday.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDateToday.Location = new System.Drawing.Point(676, 12);
+            this.lblDateToday.Name = "lblDateToday";
+            this.lblDateToday.Size = new System.Drawing.Size(84, 19);
+            this.lblDateToday.TabIndex = 2;
+            this.lblDateToday.Text = "Günün Tarihi";
+            // 
+            // guna2HtmlLabel10
+            // 
+            this.guna2HtmlLabel10.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel10.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.guna2HtmlLabel10.Location = new System.Drawing.Point(592, 12);
+            this.guna2HtmlLabel10.Name = "guna2HtmlLabel10";
+            this.guna2HtmlLabel10.Size = new System.Drawing.Size(43, 19);
+            this.guna2HtmlLabel10.TabIndex = 2;
+            this.guna2HtmlLabel10.Text = "Tarih :";
+            // 
+            // leftPanelMenuLogo
+            // 
+            this.leftPanelMenuLogo.BackColor = System.Drawing.Color.Transparent;
+            this.leftPanelMenuLogo.Image = global::WeightGain.UI.Properties.Resources.WGLogo;
+            this.leftPanelMenuLogo.ImageRotate = 0F;
+            this.leftPanelMenuLogo.Location = new System.Drawing.Point(6, 12);
+            this.leftPanelMenuLogo.Name = "leftPanelMenuLogo";
+            this.leftPanelMenuLogo.Size = new System.Drawing.Size(309, 339);
+            this.leftPanelMenuLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.leftPanelMenuLogo.TabIndex = 9;
+            this.leftPanelMenuLogo.TabStop = false;
+            this.leftPanelMenuLogo.UseTransparentBackground = true;
+            // 
+            // guna2HtmlLabel11
+            // 
+            this.guna2HtmlLabel11.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel11.Font = new System.Drawing.Font("Segoe Script", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel11.Location = new System.Drawing.Point(80, 241);
+            this.guna2HtmlLabel11.Name = "guna2HtmlLabel11";
+            this.guna2HtmlLabel11.Size = new System.Drawing.Size(155, 27);
+            this.guna2HtmlLabel11.TabIndex = 10;
+            this.guna2HtmlLabel11.Text = "Sağlıkla Kilo Alın";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(908, 500);
-            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2HtmlLabel11);
+            this.Controls.Add(this.leftPanelMenuLogo);
+            this.Controls.Add(this.txtGetCalory);
+            this.Controls.Add(this.txtBMH);
+            this.Controls.Add(this.txtHeightText);
+            this.Controls.Add(this.txtWeightText);
+            this.Controls.Add(this.txtAgeText);
+            this.Controls.Add(this.guna2HtmlLabel8);
+            this.Controls.Add(this.guna2HtmlLabel7);
+            this.Controls.Add(this.guna2HtmlLabel6);
+            this.Controls.Add(this.guna2HtmlLabel5);
+            this.Controls.Add(this.guna2HtmlLabel10);
+            this.Controls.Add(this.lblDateToday);
+            this.Controls.Add(this.guna2HtmlLabel3);
+            this.Controls.Add(this.guna2HtmlLabel4);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.leftPanelMenuLogo)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblUser;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel4;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel5;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel6;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel7;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel8;
+        private System.Windows.Forms.TextBox txtAgeText;
+        private System.Windows.Forms.TextBox txtWeightText;
+        private System.Windows.Forms.TextBox txtHeightText;
+        private System.Windows.Forms.TextBox txtBMH;
+        private System.Windows.Forms.TextBox txtGetCalory;
+        private Guna.UI2.WinForms.Guna2HtmlLabel lblDateToday;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel10;
+        private Guna.UI2.WinForms.Guna2PictureBox leftPanelMenuLogo;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel11;
     }
 }
--- WeightGain.UI/UserForms/MainForm.cs
+++ WeightGain.UI/UserForms/MainForm.cs
@@ -7,14 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WeightGain.DAL.Repositories;
+using WeightGain.DATA;
 
 namespace WeightGain.UI.UserForms
 {
     public partial class MainForm : Form
     {
-        public MainForm()
+        private readonly User _logginedUser;
+        public MainForm(User logginedUser)
         {
+            _logginedUser = logginedUser;
             InitializeComponent();
+           
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            lblDateToday.Text = DateTime.Now.ToString();
+            decimal weight = _logginedUser.Weight;
+            decimal height = _logginedUser.Height;
+            byte age= _logginedUser.Age;
+            lblUser.Text = _logginedUser.FullName;
+            txtAgeText.Text = Convert.ToString(age);
+            txtHeightText.Text = Convert.ToString(height);
+            txtWeightText.Text = Convert.ToString(weight);
+
+            txtBMH.Text =Convert.ToString( weight / height * height) ;
+            double neededCalory = (655.1+(9.56 *(double)weight)+(1.85*(double)height)-(4.68*age))*1.1;
+            txtGetCalory.Text = neededCalory.ToString();
         }
     }
 }
