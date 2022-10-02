5ca1f1bdc024440c387bb4a6e9e14a8ae276f81a
Çağatay Korkut <korkutc4@gmail.com>
2022-10-02 21:36:02 +0300

    .

--- WeightGain.UI/UserForms/ReportForm.cs
+++ WeightGain.UI/UserForms/ReportForm.cs
@@ -11,6 +11,7 @@ namespace WeightGain.UI.UserForms
     public partial class ReportForm : Form
     {
         private readonly MealTimeRepository _mealTimeRepository;
+        private readonly ExerciseRepository _exerciseRepository;
         private readonly User _logginedUser;
 
         public ReportForm(User logginedUser)
@@ -18,6 +19,7 @@ namespace WeightGain.UI.UserForms
             InitializeComponent();
             _logginedUser = logginedUser;
             _mealTimeRepository = new MealTimeRepository();
+            _exerciseRepository = new ExerciseRepository();
             dtpArchiveStartDate.MaxDate = DateTime.Now;
             dtpArchiveStartDate.Value = DateTime.Now;
         }
@@ -34,6 +36,13 @@ namespace WeightGain.UI.UserForms
             var secondSnackMeals = new List<string>();
             var dinnerMeals = new List<string>();
             var lastSnackMeals = new List<string>();
+
+            var breakfastCalory = 0m;
+            var firstSnackCalory = 0m;
+            var lunchCalory = 0m;
+            var secondSnackCalory = 0m;
+            var dinnerCalory = 0m;
+            var lastSnackCalory = 0m;
             switch (selectedToDate)
             {
                 case 0: // Günlük
@@ -45,24 +54,54 @@ namespace WeightGain.UI.UserForms
                         {
                             case MealTimeEnum.Breakfast:
                                 breakfastMeals.AddRange(products.Select(x => x.ProductName));
+                                breakfastCalory += (decimal)mealTime.Products.Sum(x => x.ProductPortions.Sum(y => y.Product.Calory * y.Size));
                                 break;
                             case MealTimeEnum.FirstSnack:
                                 firstSnackMeals.AddRange(products.Select(x => x.ProductName));
+                                firstSnackCalory += (decimal)mealTime.Products.Sum(x => x.ProductPortions.Sum(y => y.Product.Calory * y.Size));
                                 break;
                             case MealTimeEnum.Lunch:
                                 lunchMeals.AddRange(products.Select(x => x.ProductName));
+                                lunchCalory += (decimal)mealTime.Products.Sum(x => x.ProductPortions.Sum(y => y.Product.Calory * y.Size));
                                 break;
                             case MealTimeEnum.SecondSnack:
                                 secondSnackMeals.AddRange(products.Select(x => x.ProductName));
+                                secondSnackCalory += (decimal)mealTime.Products.Sum(x => x.ProductPortions.Sum(y => y.Product.Calory * y.Size));
                                 break;
                             case MealTimeEnum.Dinner:
                                 dinnerMeals.AddRange(products.Select(x => x.ProductName));
+                                dinnerCalory += (decimal)mealTime.Products.Sum(x => x.ProductPortions.Sum(y => y.Product.Calory * y.Size));
                                 break;
                             case MealTimeEnum.LastSnack:
                                 lastSnackMeals.AddRange(products.Select(x => x.ProductName));
+                                lastSnackCalory += (decimal)mealTime.Products.Sum(x => x.ProductPortions.Sum(y => y.Product.Calory * y.Size));
                                 break;
                         }
                     }
+                    txtBreakFast.Text = breakfastCalory.ToString();
+                    txtFirstSnack.Text = firstSnackCalory.ToString();
+                    txtLunch.Text= lunchCalory.ToString();
+                    txtSecondSnack.Text= secondSnackCalory.ToString();
+                    txtDinner.Text= dinnerCalory.ToString();
+                    txtThirdSnack.Text=lastSnackCalory.ToString();
+                    var totalDailyCalory= breakfastCalory+firstSnackCalory+lunchCalory+secondSnackCalory+dinnerCalory+lastSnackCalory;
+                    txtTotalCalory.Text= totalDailyCalory.ToString();
+
+                    var weight = _logginedUser.Weight;
+                    var height = _logginedUser.Height;
+                    var age = _logginedUser.Age;
+                    var neededCalory = decimal.Round((655.1m + (9.56m * weight) + (1.85m * height) - (4.68m * age)) * 1.1m, 2);
+
+                    var dailyExercises = _exerciseRepository.GetByDate(selectedDate, _logginedUser.Id);
+                    if (dailyExercises != null)
+                    {
+                        foreach (var dailyExercise in dailyExercises)
+                        {
+                            neededCalory += (decimal)dailyExercise.TotalCal;
+                        }
+                    }
+                    txtTotalNeedCalory.Text= neededCalory.ToString();
+
                     lblMorning.Text = string.Join(", ", breakfastMeals);
                     lblFirstSnack.Text = string.Join(", ", firstSnackMeals);
                     lblAfternoon.Text = string.Join(", ", lunchMeals);
